f24c8bf75bd3a56780752e6373f90e6e8359eb20
OSERHIRDEV <oserhir10@gmail.com>
2022-06-06 17:21:02 +0100

    if user click to the add button

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const input_todo = document.getElementById("input-todo"); // getting user entere
 const display = document.getElementById("display");
 const count = document.getElementById("count");
 const clearAll = document.querySelector(".clearAll");
+const btnAdd = document.getElementsByClassName("fa-solid fa-circle-plus")[0]; //
+
+let tasks = [];
 
 // active - unactive the add button
 input_todo.addEventListener("keyup", () => {
@@ -13,4 +16,21 @@ input_todo.addEventListener("keyup", () => {
   } else {
     btnAdd.classList.remove("active"); // unactive add btn
   }
+});
+
+// if user click to the add button
+btnAdd.addEventListener("click", () => {
+  let userData = input_todo.value; // Getting User input Value
+  // Getting LocalStorage
+  let getLocalStorage = localStorage.getItem("todoliststorage");
+  // Checking if LocalStorage = Null
+  if (getLocalStorage == null) {
+    tasks = []; // Create Blank ARRAYS
+  } else {
+    tasks = JSON.parse(getLocalStorage);
+  }
+
+  tasks.push(userData); // add THE user
+  localStorage.setItem("todoliststorage", JSON.stringify(tasks)); // transform json objet into json string
+  input_todo.value = "";
 });
