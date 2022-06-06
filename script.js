32478771d03a96d8064c4565cd5056c07fafdd7f
OSERHIRDEV <oserhir10@gmail.com>
2022-06-06 17:11:27 +0100

    active - unactive the add button

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const btnAdd = document.getElementsByClassName("fa-solid fa-circle-plus")[0];
 const display = document.getElementById("display");
 const count = document.getElementById("count");
 const clearAll = document.querySelector(".clearAll");
+
+// active - unactive the add button
+input_todo.addEventListener("keyup", () => {
+  if (input_todo.value.trim() != 0) {
+    btnAdd.classList.add("active");
+  } else {
+    btnAdd.classList.remove("active");
+  }
+});
