fca296ddb31b2db6c25317f827813c1c3810435c
OSERHIRDEV <oserhir10@gmail.com>
2022-06-06 17:09:14 +0100

    getting all required elements

---
+++ script.js
@@ -0,0 +1,7 @@
+// getting all required elements
+const btnRemove = document.getElementById("remove");
+const input_todo = document.getElementById("input-todo");
+const btnAdd = document.getElementsByClassName("fa-solid fa-circle-plus")[0];
+const display = document.getElementById("display");
+const count = document.getElementById("count");
+const clearAll = document.querySelector(".clearAll");
