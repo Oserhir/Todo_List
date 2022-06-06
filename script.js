3bf1d9d32f252c9981d7469a34305befdded66a2
OSERHIRDEV <oserhir10@gmail.com>
2022-06-06 17:13:54 +0100

    update active - unactive the add button

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // getting all required elements
 const btnRemove = document.getElementById("remove");
-const input_todo = document.getElementById("input-todo");
-const btnAdd = document.getElementsByClassName("fa-solid fa-circle-plus")[0];
+const input_todo = document.getElementById("input-todo"); // getting user entered value
 const display = document.getElementById("display");
 const count = document.getElementById("count");
 const clearAll = document.querySelector(".clearAll");
@@ -9,8 +8,9 @@ const clearAll = document.querySelector(".clearAll");
 // active - unactive the add button
 input_todo.addEventListener("keyup", () => {
   if (input_todo.value.trim() != 0) {
-    btnAdd.classList.add("active");
+    // check if value aren't only space
+    btnAdd.classList.add("active"); // active add btn
   } else {
-    btnAdd.classList.remove("active");
+    btnAdd.classList.remove("active"); // unactive add btn
   }
 });
